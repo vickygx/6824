7db46a2a967b028ec5a71adb562b4266d7d8b348
Vicky Gong <vickyg@mit.edu>
2016-02-05 16:37:11 -0500

    Possible working solution for doMap

--- src/mapreduce/common_map.go
+++ src/mapreduce/common_map.go
@@ -1,8 +1,19 @@
 package mapreduce
 
+
 import (
 	"hash/fnv"
+	"io/ioutil"
+	"encoding/json"
+	"os"
+	// "fmt"
 )
+
+func check_err(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 // doMap does the job of a map worker: it reads one of the input files
 // (inFile), calls the user-defined map function (mapF) for that file's
@@ -14,32 +25,36 @@ func doMap(
 	nReduce int, // the number of reduce task that will be run ("R" in the paper)
 	mapF func(file string, contents string) []KeyValue,
 ) {
-	// TODO:
-	// You will need to write this function.
-	// You can find the filename for this map task's input to reduce task number
-	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
-	// below doMap) should be used to decide which file a given key belongs into.
-	//
-	// The intermediate output of a map task is stored in the file
-	// system as multiple files whose name indicates which map task produced
-	// them, as well as which reduce task they are for. Coming up with a
-	// scheme for how to store the key/value pairs on disk can be tricky,
-	// especially when taking into account that both keys and values could
-	// contain newlines, quotes, and any other character you can think of.
-	//
-	// One format often used for serializing data to a byte stream that the
-	// other end can correctly reconstruct is JSON. You are not required to
-	// use JSON, but as the output of the reduce tasks *must* be JSON,
-	// familiarizing yourself with it here may prove useful. You can write
-	// out a data structure as a JSON string to a file using the commented
-	// code below. The corresponding decoding functions can be found in
-	// common_reduce.go.
-	//
-	//   enc := json.NewEncoder(file)
-	//   for _, kv := ... {
-	//     err := enc.Encode(&kv)
-	//
-	// Remember to close the file after you have written all the values!
+	var contentsByte, err = ioutil.ReadFile(inFile)
+	check_err(err)
+	kvpairs := mapF(inFile, string(contentsByte))
+
+	// Mapping filename to list of kv pairs
+	filename_to_kv := make(map[string][]KeyValue)
+
+	// Loop through each kv pair
+	for _, kv := range kvpairs {
+		// Figure out where the kv pair belongs to
+		r := int(ihash(kv.Key)) % nReduce
+		filename := reduceName(jobName, mapTaskNumber, int(r))
+
+		// Add it to the map
+		filename_to_kv[filename] = append(filename_to_kv[filename], kv)
+	}
+
+	// Loop through filenames and create
+    for filename := range filename_to_kv {
+    	f, err := os.Create(filename)
+		defer f.Close()
+		check_err(err)
+
+		encoder := json.NewEncoder(f)
+
+		for _, kv := range filename_to_kv[filename] {
+			err := encoder.Encode(&kv)
+			check_err(err) // TODO: maybe change
+		}
+    }
 }
 
 func ihash(s string) uint32 {
